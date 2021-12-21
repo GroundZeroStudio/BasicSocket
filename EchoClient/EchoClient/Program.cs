f62518fc84858e750c6c70258b5efcc1378c0ef7
CaoLi <morganfreemanX@outlook.com>
2021-12-21 12:48:44 +0800

    BasicSocket
    KPI

---
+++ EchoClient/EchoClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static Socket mSocket;
+        static byte[] mReadBuff = new byte[1024];
+        static string mReceiveMsg = string.Empty;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Connection();
+            Console.ReadLine();
+        }
+
+        static void Connection()
+        {
+            // 新建
+            mSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // 建立连接
+            mSocket.BeginConnect("127.0.0.1", 8888, ConnectionCallBack, mSocket);
+        }
+
+        static void ConnectionCallBack(IAsyncResult rResult)
+        {
+            try
+            {
+                Socket rSocket = (Socket)rResult.AsyncState;
+                rSocket.EndConnect(rResult);
+                Console.WriteLine("socket connect success");
+                rSocket.BeginReceive(mReadBuff, 0, 1024, 0, ReceiveCallBack, rSocket);
+                Send("测试服务器连接1"); 
+                Send("测试服务器连接2");
+            }
+            catch
+            {
+                Console.WriteLine("socket connect fail");
+            }
+        }
+
+        static void Send(string rString)
+        {
+            byte[] rSendBytes = System.Text.Encoding.Default.GetBytes(rString);
+            
+            // 发送
+            mSocket.BeginSend(rSendBytes, 0, rSendBytes.Length, 0, SendCallBack, mSocket);
+            
+            // 关闭
+            //mSocket.Close();
+        }
+
+        static void SendCallBack(IAsyncResult rResult)
+        {
+            try
+            {
+                Socket rSocket = (Socket)rResult.AsyncState;
+                int nCount = rSocket.EndSend(rResult);
+                Console.WriteLine("Send Success");
+            }
+            catch
+            {
+                Console.WriteLine("Send fail");
+            }
+        }
+
+        static void ReceiveCallBack(IAsyncResult rResult)   
+        {
+            try
+            {
+                Socket rSocket = (Socket)rResult.AsyncState;
+                int nCount = rSocket.EndReceive(rResult);
+                mReceiveMsg = System.Text.Encoding.Default.GetString(mReadBuff, 0, nCount);
+                rSocket.BeginReceive(mReadBuff, 0, 1024, 0, ReceiveCallBack, rSocket);
+                Console.WriteLine(mReceiveMsg);
+            }
+            catch
+            {
+                Console.WriteLine("Receive fail");
+            }
+        }
+    }
+}
---
+++ EchoServer/EchoServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static Socket mListenSocket;
+        static Dictionary<Socket, ClientState> mClients = new Dictionary<Socket, ClientState>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Echo Server Enter");
+
+            // 创建socket
+            mListenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress rIpAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint rIpEndPoint = new IPEndPoint(rIpAdr, 8888);
+
+            // 绑定地址和端口
+            mListenSocket.Bind(rIpEndPoint);
+
+            // 监听
+            mListenSocket.Listen(0);
+            Console.WriteLine("服务器启动成功");
+            mListenSocket.BeginAccept(AcceptCallback, mListenSocket);
+            Console.ReadLine();
+        }
+
+        static void AcceptCallback(IAsyncResult rResult)
+        {
+            Console.WriteLine("服务器Accept");
+
+            Socket rListenSocket = (Socket)rResult.AsyncState;
+            Socket rClientSocket = rListenSocket.EndAccept(rResult);
+            ClientState rClientState = new ClientState();
+            rClientState.mSocket = rClientSocket;
+            mClients.Add(rClientSocket, rClientState);
+            rClientSocket.BeginReceive(rClientState.readBuff, 0, 1024, SocketFlags.None, ReceiveCallback, rClientState);
+            rListenSocket.BeginAccept(AcceptCallback, rListenSocket);
+
+        }
+
+        static void ReceiveCallback(IAsyncResult rResult)
+        {
+            ClientState rClientState = (ClientState)rResult.AsyncState;
+            Socket rClientSocket = rClientState.mSocket;
+            int nCount = rClientState.mSocket.EndReceive(rResult);
+        
+            if(nCount == 0)
+            {
+                rClientSocket.Close();
+                mClients.Remove(rClientSocket);
+                Console.WriteLine("Socket Close");
+                return;
+            }
+
+            string rReadStr = System.Text.Encoding.Default.GetString(rClientState.readBuff, 0, nCount);
+
+            Console.WriteLine("服务器接收" + rReadStr);
+            byte[] rSendBytes = System.Text.Encoding.Default.GetBytes(rReadStr);
+            rClientSocket.Send(rSendBytes);
+        }
+    }
+
+    class ClientState
+    {
+        public Socket mSocket;
+        public byte[] readBuff = new byte[1024];
+    }
+}
